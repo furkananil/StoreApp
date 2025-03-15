ec205fe5898171715565732b674585d8ee5e0af0
furkananil <furkananil960@gmail.com>
2025-03-15 21:22:56 +0300

    Data Seeding

---
+++ StoreApp.Web/Migrations/20250315182019_Init.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StoreApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Telefon", "guzel telefon", "Samsung S24", 40000m },
+                    { 2, "Telefon", "guzel telefon", "Samsung S25", 50000m },
+                    { 3, "Telefon", "guzel telefon", "Samsung S26", 60000m },
+                    { 4, "Telefon", "guzel telefon", "Samsung S27", 70000m },
+                    { 5, "Telefon", "guzel telefon", "Samsung S28", 80000m },
+                    { 6, "Telefon", "guzel telefon", "Samsung S29", 90000m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
